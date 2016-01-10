fe79d0a7777addc309e13d68973a4b55ff8cadb2
stivensernas <stivenserna03@gmail.com>
2016-01-09 22:29:41 -0500

    Actualizacion del login

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>login Admin</title>
+
+	<title>GYM FORMAS RENACER</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-
 	<link rel="stylesheet" href="css/bootstrap.min.css">
-
+	<link rel="stylesheet" href="css/fondo_inicio.css" type="text/css">
 </head>
 <body>
 
@@ -21,8 +22,22 @@
 <div class="col-md-4"></div>
 
 <div class="col-md-4">
+	
+	<img class="#contenedor"src="g.jpg" alt="">
+</div>
 
-			<h1 class="h1" style="color:black">Login</h1>
+<div class="col-md-4"></div>
+	
+
+</div>
+
+<div class="row">
+
+<div class="col-md-4"></div>
+
+<div class="col-md-4">
+
+			<h1 class="h1" style="color:red">GYM FORMAS RENACER</h1>
 			<form method="post" action="validar.php">
 				<input type="text" class="form-control" name="user" autocomplete="off" required><br><br>
 				<input type="password" class="form-control" name="pw" autocomplete="off" required><br><br>
--- panel.php
+++ panel.php
@@ -6,27 +6,25 @@ if(isset($_SESSION['user'])) {?>
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Dashboard</title>
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-
+    <title>Dashboard</title>
+  
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
 </head>
 <body>
-
-
 <div class="col-md-4"></div>
 
-<div class="col-md-4">
-<img src="logym.JPEG" alt="" class="img-circle">
-      
-</div>
-<div class="col-md-4"></div>
+      <div class="col-md-4">
+        <img src="logym.JPEG" alt="" class="img-circle">
       </div>
+      <div class="col-md-4"></div>
+      
+ </div>
 
-
-<img src="logym.jpeg" alt="" class="img-circle">
-  <a href="logout.php"><button>Salir</button></a>
+    <img src="logym.jpeg" alt="" class="img-circle"> <br>
+    <a href="logout.php"><button>Salir</button></a>
 </div>
 </body>
 </html>
